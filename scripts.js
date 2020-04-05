366c56b54ae690ec69ea6085fe9728543915dee3
Sokrady Chey <sokradychey@gmail.com>
2020-04-05 13:22:41 -0400

    JS updates to Memory Card board

--- scripts.js
+++ scripts.js
@@ -1,59 +1,57 @@
-// scripts.js
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
 
-function flipCard() {
+  function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
 
-    this.classList.add('flip')
+    this.classList.add('flip');
 
     if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        return;
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
     }
 
     secondCard = this;
+    lockBoard = true;
 
     checkForMatch();
-}
+  }
 
-function checkForMatch() {
+  function checkForMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
     isMatch ? disableCards() : unflipCards();
-}
+  }
 
-function disableCards() {
+  function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
-}
+  }
 
-function unflipCards() {
-    lockBoard = true;
-
+  function unflipCards() {
     setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        
-        resetBoard();
-    }, 1500);
-}
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
 
-function resetBoard() {
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-}
+  }
 
 (function shuffle() {
     cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() *12);
-        card.style.order = randomPos;
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
     });
 })();
 
