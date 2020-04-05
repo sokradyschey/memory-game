3d974de0be3750819c224a6ce4682a3f0b8a0df1
Sokrady Chey <sokradychey@gmail.com>
2020-04-05 00:28:34 -0400

    Updated JS

--- scripts.js
+++ scripts.js
@@ -2,9 +2,11 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
     this.classList.add('flip')
 
     if (!hasFlippedCard) {
@@ -20,12 +22,8 @@ function flipCard() {
 }
 
 function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();
-        return;
-    }
-
-    unflipCards();
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
@@ -34,9 +32,12 @@ function disableCards() {
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard = false;
     }, 1500);
 }
 
