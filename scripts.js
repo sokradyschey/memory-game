64d1f8eb9d7e68bf58fc21ef71cc7abb2537a33b
Sokrady Chey <sokradychey@gmail.com>
2020-04-05 00:09:03 -0400

    Card Flip JS

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip')
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
