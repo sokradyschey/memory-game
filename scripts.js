a1abc04970428704e984416292d304740ec47d62
Sokrady Chey <sokradychey@gmail.com>
2020-04-05 13:11:44 -0400

    Shuffle Board

--- scripts.js
+++ scripts.js
@@ -50,4 +50,11 @@ function resetBoard() {
     [firstCard, secondCard] = [null, null];
 }
 
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() *12);
+        card.style.order = randomPos;
+    });
+})();
+
 cards.forEach(card => card.addEventListener('click', flipCard));
